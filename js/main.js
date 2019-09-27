dd6879e27c1d7005019bf921468fac38b185d0f3
Adamu Fura Suleiaman <adamufura98@gmail.com>
2019-09-27 00:35:00 -0700

    New Files Added
    I just add the files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* ==================================================================================================
+Reach the Front end developer here => +2348166644083
+Subscribe to our channel on youtube @ IT WORKS CHANNEL
+HELP ME REACH 1000 SUBSCRIBERS PLEASE
+===================================================================================================*/
+/*
+You can remove alert because its annoying to other users
+*/
+/*window.alert("FILL ALL REQUIRED FIELDS BEFORE 2 MINS OTHERWISE THE PAGE WILL RESET THE DATA");
+window.alert("Press Ok to continue"); */
+
+function show_current_date() {
+    var show_date = new Date();
+    document.getElementById('header-date').innerHTML =  " Student Biodata Form  " + show_date.getDay() + " / " + show_date.getMonth() + " / " + show_date.getFullYear();
+}
